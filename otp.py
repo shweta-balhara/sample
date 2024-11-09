0abd7b5454e0cd736682351e1276c5ee4daffacb
shweta-balhara <shwetabalhara1405@gmail.com>
2024-11-09 14:52:45 +0530

    Add files via upload

---
+++ otp.py
@@ -0,0 +1,69 @@
+
+import random
+
+def otp():
+    x = 0;
+    x = random.randint(000,999)
+    
+    if x > 0:
+        return(x)
+    
+    else:
+        print()
+    print(x)    
+otp()
+
+
+
+#import random
+#def onetime():
+#    x = random.randint(000,999)
+#    print("otp", x)
+#    password = input("enter otp:")
+#    if int(password) == x :
+#        print("logined")
+#    else:
+#        print("again")
+#onetime()
+
+
+#import random
+
+#def opt():
+#    x = 0;
+#    x = random.randint(000,999)
+#    if x > 0:
+#        return()
+#    else:
+#        opt()
+#    print(x)
+
+
+# import random
+
+# def onetime1():
+#     x = random.randint(000,999)
+#     print("otp",x)
+#     password = input("enter otp here:")
+#     if int(password) == x:
+#         print("logined")
+#     else:
+#         def onetime2():
+#             x = random.randint(000,999)
+#             print("otp",x)
+#             password = input("enter otp here:")
+#             if int(password) == x:
+#                 print("logined")
+#             else:
+#                 def onetime3():
+#                     x = random.randint(000,999)
+#                     print("otp",x)
+#                     password = input("enter otp here:")
+#                     if int(password) == x:
+#                         print("logined")
+#                     else:
+#                         print("try again")
+#                 onetime3()
+#         onetime2()
+# onetime1()    
+	
